4f2975be1ca8d357ebb8114fb294526d50c201c1
Christian Ng <christianr.ng@gmail.com>
2015-08-25 13:40:51 -0700

    switch to Selenium

--- Scraper.py
+++ Scraper.py
@@ -1,19 +1,15 @@
-import requests
-
-username = raw_input("Username:\n")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#Get user's credentials
+username = raw_input("Username:\n") 
 password = raw_input("Password:\n")
-school = raw_input("School specific login URL")
 
-payload = {
-    'inUserName': username,
-    'inUserPass': password
-}
+driver = webdriver.Firefox() #Should switch to Phantomjs
+driver.get("http://calhigh.schoolloop.com/portal/login") #Go to website
+elem = driver.find_element_by_name("login_name") #Find username form
+elem.send_keys(username) #Type in username
+elem = driver.find_element_by_name("password") #Find password form
+elem.send_keys(password) #Type in password
+elem.send_keys(Keys.RETURN) #submit
+assert "No results found." not in driver.page_source
 
-with requests.Session() as s:
-    p = s.post(school, data=payload)
-    # print the html returned to see if it's a successful login page.
-    print p.text
-
-    r = s.get(school) #getting a bad request error here
-    print r.text
-        
